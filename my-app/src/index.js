70cbce8ee0757f3ec2d54025ad210e5075a7f29b
blair06 <hyewon980610@likelion.org>
2020-01-01 20:13:03 +0900

    jsx를 이용해 시간 출력하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -30,7 +30,19 @@ const element = <h1>Hello, {formatName(name)}!</h1>
 ReactDOM.render(
     element,
     document.getElementById('root'));
-
+//시계 출력하기
+function tick() {
+    const element = (
+        <div>
+            <h1>Hello, World!</h1>
+            <h2>It is{new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+    ReactDOM.render(
+        element,
+        document.getElementById('root'));
+}
+setInterval(tick, 1000);
 
 
 
